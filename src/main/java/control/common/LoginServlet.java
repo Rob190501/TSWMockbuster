5d0500c0c245d1b901e435c0f6032fa33dbed6d4
Rob190501 <95653586+Rob190501@users.noreply.github.com>
2024-06-07 16:26:34 +0200

    Aggiunto hash delle password

--- src/main/java/control/common/LoginServlet.java
+++ src/main/java/control/common/LoginServlet.java
@@ -1,6 +1,7 @@
 package control.common;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,7 +44,7 @@ public class LoginServlet extends HttpServlet {
 		}
 		
 		username = username.trim();
-		password = password.trim();
+		password = toHash(password.trim());
 		
 		if(isAdmin(username, password)) {
 			request.getSession().setAttribute("isAdmin", Boolean.TRUE);
@@ -61,11 +62,26 @@ public class LoginServlet extends HttpServlet {
 	}
 	
 	private Boolean isAdmin(String username, String password) {
-		return username.equals("admin") && password.equals("password");
+		return username.equals("admin") && password.equals("b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86");
 	}
 	
 	private Boolean isUser(String username, String password) {
-		return username.equals("user") && password.equals("password");
+		return username.equals("user") && password.equals("b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86");
 	}
-
+	
+	private String toHash(String password) {
+		String hashString = null;
+		try {
+			java.security.MessageDigest digest = java.security.MessageDigest.getInstance("SHA-512");
+			byte[] hash = digest.digest (password.getBytes(StandardCharsets.UTF_8));
+			hashString = "";
+			for (int i = 0; i < hash.length; i++) {
+				hashString += Integer.toHexString((hash[i] & 0xFF) | 0x100).substring(1,3);
+			}
+		}
+		catch(java.security.NoSuchAlgorithmException e) {
+			System.out.println(e);
+		}
+		return hashString;
+	}
 }
